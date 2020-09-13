3b7dd1f07659b4f50c9be64d621130a254dbf3e7
Ozairfzn <71073358+Ozairfzn@users.noreply.github.com>
2020-09-13 14:44:00 +0200

    presque finit le jeu de la vie

---
+++ OC Informatique/Arithmétique/Activités.py
@@ -0,0 +1,64 @@
+from math import *
+import time
+
+
+def premier(p):
+    if p == 2:
+        return True
+    elif p < 2:
+        return False
+    elif p % 2 == 0:
+        return False
+    n = int(p**(0.5))
+    for i in range(3, n, 2):
+        if p % i == 0:
+            return False
+    return True
+
+
+def nombre_solutions_goldbach(n):
+    for p in range(2, n//2+1):
+        if premier(p):
+            q = n - p
+            if p <= q and premier(q):
+                print(f"{n} = {p} + {q}")
+
+
+def nombre_de_diviseurs(n):
+    L = []
+    for i in range(1, n//2+1):
+        if n % i == 0:
+            L.append(i)
+    L.append(n)
+    return len(L)
+
+
+def quatre_et_huit_diviseurs(Nmin, Nmax):
+    n4 = 0
+    n8 = 0
+    for i in range(Nmin, Nmax+1):
+        if nombre_de_diviseurs(i) == 4:
+            n4 += 1
+        elif nombre_de_diviseurs(i) == 8:
+            n8 += 1
+    return(n4, n8)
+
+
+def racine(n):
+    return floor(sqrt(n))
+
+
+def racine_2(n):
+    p = 0
+    while True:
+        if p**2 >= n:
+            return p-1
+        p += 1
+
+
+def racine_3(n):
+    a, b = 1, n
+    while abs(a-b) > 1:
+        a = (a+b)//2
+        b = n//a
+    return min(a, b)
---
+++ OC Informatique/Jeu De La Vie/Jeu.py
@@ -0,0 +1,150 @@
+from tkinter import *
+from random import *
+
+hauteur = 800
+taille = 5
+
+
+def nombre_voisins(i, j, tab):
+    voisins = 0
+    # LES 4 COINS
+    if i == 0 and j == 0:
+        if tab[i][j+1] == 1:
+            voisins += 1
+        if tab[i+1][j+1] == 1:
+            voisins += 1
+        if tab[i+1][j] == 1:
+            voisins += 1
+    elif i == taille-1 and j == taille-1:
+        if tab[i][j-1] == 1:
+            voisins += 1
+        if tab[i-1][j-1] == 1:
+            voisins += 1
+        if tab[i-1][j] == 1:
+            voisins += 1
+    elif i == taille-1 and j == 0:
+        if tab[i][j+1] == 1:
+            voisins += 1
+        if tab[i-1][j+1] == 1:
+            voisins += 1
+        if tab[i-1][j] == 1:
+            voisins += 1
+    elif i == 0 and j == taille-1:
+        if tab[i][j-1] == 1:
+            voisins += 1
+        if tab[i+1][j-1] == 1:
+            voisins += 1
+        if tab[i+1][j] == 1:
+            voisins += 1
+    # LES 4 LINES
+    elif i == 0:
+        if tab[i][j-1] == 1:
+            voisins += 1
+        if tab[i][j+1] == 1:
+            voisins += 1
+        if tab[i+1][j-1] == 1:
+            voisins += 1
+        if tab[i+1][j] == 1:
+            voisins += 1
+        if tab[i+1][j+1] == 1:
+            voisins += 1
+    elif i == taille-1:
+        if tab[i][j-1] == 1:
+            voisins += 1
+        if tab[i][j+1] == 1:
+            voisins += 1
+        if tab[i-1][j-1] == 1:
+            voisins += 1
+        if tab[i-1][j] == 1:
+            voisins += 1
+        if tab[i-1][j+1] == 1:
+            voisins += 1
+    elif j == 0:
+        if tab[i-1][j] == 1:
+            voisins += 1
+        if tab[i+1][j] == 1:
+            voisins += 1
+        if tab[i][j+1] == 1:
+            voisins += 1
+        if tab[i-1][j+1] == 1:
+            voisins += 1
+        if tab[i+1][j+1] == 1:
+            voisins += 1
+    elif j == taille-1:
+        if tab[i-1][j] == 1:
+            voisins += 1
+        if tab[i+1][j] == 1:
+            voisins += 1
+        if tab[i][j-1] == 1:
+            voisins += 1
+        if tab[i-1][j-1] == 1:
+            voisins += 1
+        if tab[i+1][j-1] == 1:
+            voisins += 1
+    # CENTRE
+    else:
+        if tab[i][j-1] == 1:
+            voisins += 1
+        if tab[i][j+1] == 1:
+            voisins += 1
+        if tab[i-1][j-1] == 1:
+            voisins += 1
+        if tab[i-1][j] == 1:
+            voisins += 1
+        if tab[i-1][j+1] == 1:
+            voisins += 1
+        if tab[i+1][j+1] == 1:
+            voisins += 1
+        if tab[i+1][j-1] == 1:
+            voisins += 1
+        if tab[i+1][j] == 1:
+            voisins += 1
+    return voisins
+
+
+def evolution(tableau):
+    tab = [[0 for j in range(taille)]
+           for i in range(taille)]
+    for i in range(taille):
+        for j in range(taille):
+            if nombre_voisins(i, j, tableau) == 3:
+                tab[i][j] = 1
+            elif nombre_voisins(i, j, tableau) == 2 and tableau[i][j] == 1:
+                tab[i][j] = 1
+            else:
+                tab[i][j] = 0
+    return tab
+
+
+def tableau(tab):
+    echelle = hauteur//taille
+    for i in range(taille):
+        for j in range(taille):
+            if tab[i][j] == 1:
+                canvas.create_rectangle(
+                    i*echelle, j*echelle, (i+1)*echelle, (j+1)*echelle, fill="red")
+            else:
+                canvas.create_rectangle(
+                    i*echelle, j*echelle, (i+1)*echelle, (j+1)*echelle,)
+
+
+nouveau_tableau = [[randint(0, 1) for j in range(taille)]
+                   for i in range(taille)]
+
+
+def action_bouton():
+    canvas.delete("all")
+
+    return
+
+
+root = Tk()
+canvas = Canvas(root, width=hauteur, height=hauteur, background="white")
+canvas.pack(side=LEFT, padx=5, pady=5)
+
+bouton_couleur = Button(root, text="Evoluer", width=20, command=action_bouton)
+bouton_couleur.pack()
+bouton_quitter = Button(root, text="Quitter", width=20, command=root.quit)
+bouton_quitter.pack()
+
+root.mainloop()
