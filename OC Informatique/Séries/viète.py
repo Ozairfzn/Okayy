f8a084150812ce6633fa81f8f7f0f399bf199b30
Ozairfzn <71073358+Ozairfzn@users.noreply.github.com>
2020-10-22 16:51:39 +0200

    £$

--- OC Informatique/Séries/viète.py
+++ OC Informatique/Séries/viète.py
@@ -9,17 +9,17 @@ c = eval(input(f"Entrez le coefficient c de ax{carre} + bx + c: "))
 if a == 0 and b == 0 and c == 0:
     print(f"La fonction {a}x{carre} + ({b}x) + ({c}) à une infinté de zéros")
 elif a == 0 and b == 0:
-    print(f"Le zéro de la fonction {a}x{carre} + ({b}x) + ({c})  est {c}")
+    print(f"Le zéro de la fonction {a}x{carre} + ({b})x + ({c})  est {c}")
 elif a == 0:
-    print(f"Le zéro de la fonction {a}x{carre} + ({b}x) + ({c})  est {-c/b}")
+    print(f"Le zéro de la fonction {a}x{carre} + ({b})x + ({c})  est {-c/b}")
 else:
     Delta = b**2 - 4*a*c
 
     if Delta < 0:
-        print(f"La fonction {a}x{carre} + ({b}x) + ({c})  n'a pas de zéro")
+        print(f"La fonction {a}x{carre} + ({b})x + ({c})  n'a pas de zéro")
     elif Delta == 0:
         print(
-            f"Le zéro de la fonction {a}x{carre} + ({b}x) + ({c})  est {-b/(2*a)}")
+            f"Le zéro de la fonction {a}x{carre} + ({b})x + ({c})  est {-b/(2*a)}")
     else:
         print(
-            f"Les zéros de la fonction {a}x{carre} + ({b}x) + ({c}) sont {(-b-math.sqrt(Delta))/(2*a)} et {(-b+math.sqrt(Delta))/(2*a)}")
+            f"Les zéros de la fonction {a}x{carre} + ({b})x + ({c}) sont {(-b-math.sqrt(Delta))/(2*a)} et {(-b+math.sqrt(Delta))/(2*a)}")
