d64b74b9ad57a7b586e51598a414c6c5752a1bd2
Ozairfzn <71073358+Ozairfzn@users.noreply.github.com>
2020-10-22 16:48:46 +0200

    Black Jack finit  :)

--- OC Informatique/Séries/Black_jack.py
+++ OC Informatique/Séries/Black_jack.py
@@ -13,44 +13,53 @@ print(f"Vos points: {Joueur[0]}\nLes points du croupier: {Banque[0]}")
 
 while jeu:
     tour = 0
-    while tour % 2 == 0 and joueur:
-        stop = input("Voulez-vous continuer? [oui]/non ")
-        if stop == "non":
-            joueur = False
-        Joueur.append(choice(cartes))
 
-        sommeJ = 0
-        for i in Joueur:
-            sommeJ += i
-        if sommeJ > 21:
-            jeu = False
-
-        print(f"Vos points: {sommeJ}")
+    while tour % 2 == 0:
         tour += 1
 
+        if Banque:
+            sommeB = 0
+            for i in Banque:
+                sommeB += i
+            if sommeB > 21:
+                jeu = False
+            if sommeB > 16:
+                banque = False
+                break
+            Banque.append(choice(cartes))
+            sommeB = 0
+            for i in Banque:
+                sommeB += i
 
-    while tour % 2 == 1 and banque:
-        sommeB = 0
-        if sommeB > 17:
-            banque = False 
-        Banque.append(choice(cartes))
-            
-        for i in Banque:
-            sommeB += i
-        if sommeB > 21:
-            jeu = False
-
-        print(f"Les points du croupier: {sommeB}")
+    while tour % 2 == 1:
         tour += 1
+
+        if joueur:
+            stop = input("Voulez-vous continuer? [oui]/non ")
+            if stop == "non":
+                joueur = False
+                break
+            Joueur.append(choice(cartes))
+
+            sommeJ = 0
+            for i in Joueur:
+                sommeJ += i
+            if sommeJ > 21:
+                jeu = False
+
+    print(f"Vos points: {sommeJ}")
+    print(f"Les points du croupier: {sommeB}")
+
+    if not banque and not joueur:
+        jeu = False
 
 
 if sommeB > 21:
-    print("Vous avez doublé votre mise! Vous avez mtn", 2*mise)
+    print("Vous avez doublé votre mise! ", 2*mise, "CHF")
 elif sommeJ > 21:
     print("Vous avez perdu votre mise!")
 else:
     if sommeB >= sommeJ:
         print("Vous avez perdu votre mise!")
     else:
-        print("Vous avez doublé votre mise! Vous avez mtn", 2*mise)
-
+        print("Vous avez doublé votre mise! ", 2*mise, "CHF")
