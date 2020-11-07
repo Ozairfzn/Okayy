ccc9af47ef528c86b04bbb3e67782b04e8ef4484
Ozairfzn <71073358+Ozairfzn@users.noreply.github.com>
2020-11-07 17:38:59 +0100

    euclide(s)

--- OC Informatique/Semestrielle_polynomes.py
+++ OC Informatique/Semestrielle_polynomes.py
@@ -153,7 +153,7 @@ def division_euclidienne(polynome1, polynome2):
     Q = []
 
     if len(polynome1) < len(polynome2):
-        return polynome1, polynome2
+        return polynome1, polynome2, [0, 0]
 
     while len(p1) >= len(p2):
         q = [0 for i in range(len(p1))]
