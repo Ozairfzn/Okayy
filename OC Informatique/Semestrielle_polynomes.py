7483f5214ff6c3e3e4c096e6e73ec301858c0e10
Ozairfzn <71073358+Ozairfzn@users.noreply.github.com>
2020-11-08 13:28:44 +0100

    limites fraction x-> a

--- OC Informatique/Semestrielle_polynomes.py
+++ OC Informatique/Semestrielle_polynomes.py
@@ -201,6 +201,26 @@ def limites_fractions_rationnlles_infini(polynome1, polynome2):
         return polynome1[-1]/polynome2[-1]
 
 
+def limites_fractions_rationnlles(polynome1, polynome2, point):
+    if evaluer_polynome(polynome2, point) != 0:
+        return evaluer_polynome(polynome1, point) / evaluer_polynome(polynome2, point)
+
+    dx = 0.000001
+    a_plus = evaluer_polynome(polynome1, point+dx) / \
+        evaluer_polynome(polynome2, point+dx)
+    a_moin = evaluer_polynome(polynome1, point-dx) / \
+        evaluer_polynome(polynome2, point-dx)
+
+    if a_plus > 10000 and a_moin < 10000 or a_plus < 10000 and a_moin > 10000:
+        return "∄"
+    elif abs(a_plus - a_moin) < 0.1:
+        return round(a_plus + (a_plus - a_moin), 3)
+    elif a_plus > 10000 and a_moin > 10000:
+        return "∞"
+    elif a_plus < 10000 and a_moin < 10000:
+        return "-∞"
+
+
 def dérivée_polynome(polynome):
     dérivée = []
 
